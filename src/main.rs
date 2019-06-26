925192c8f1aeae42ac4f7267c867d84daea25ee2
Simon Martin <smartin@scottlogic.com>
2019-06-26 21:12:13 +0000

    Match example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-use std::fs::File;
+fn main() {
+    let number = 13;
 
-fn main() -> Result<(), std::io::Error> {
-    let _f = File::open("bar.txt").unwrap();
-
-    Ok(())
+    println!("Tell me about {}", number);
+    match number {
+        // Match a single value
+        1 => println!("One!"),
+        // Match several values
+        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
+        // Match an inclusive range
+        13...19 => println!("A teen"),
+        // Handle the rest of cases
+        _ => println!("Ain't special"),
+    }
 }
