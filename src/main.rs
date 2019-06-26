2995692cbd4e19257bf409cee43d3a8d7df6a7b3
Simon Martin <smartin@scottlogic.com>
2019-06-26 21:23:52 +0000

    Mutability borrow example

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
+fn inc(val: &mut i32) {
+    (*val) += 1;
+}
+
 fn main() {
-    let number = 13;
+    let mut number = 13;
+
+    inc(&mut number);
 
     println!("Tell me about {}", number);
-    match number {
-        // Match a single value
-        1 => println!("One!"),
-        // Match several values
-        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
-        // Match an inclusive range
-        13...19 => println!("A teen"),
-        // Handle the rest of cases
-        _ => println!("Ain't special"),
-    }
 }
