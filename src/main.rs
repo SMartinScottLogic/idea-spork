bf7048485ec02fdb5478d4a9f5a5910b3e5581b4
Simon Martin <smartin@scottlogic.com>
2019-10-08 08:36:12 +0000

    Trait method collision example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,49 @@
-fn inc(val: &mut i32) {
-    (*val) += 1;
+trait A {
+    fn name(&self);
+}
+
+trait B {
+    fn name(&self);
+}
+
+trait C {
+    fn name(&self);
+}
+
+#[derive(Debug)]
+struct D {
+    value: u8,
+    name: String
+}
+
+impl D {
+    fn new() -> D {
+        D { value: 1, name: "D".to_string() }
+    }
+}
+
+impl A for D {
+    fn name(&self) {
+        println!("A for D {:?}", self);
+    }
+}
+
+impl B for D {
+    fn name(&self) {
+        println!("B for D {:?}", self);
+    }
+}
+
+impl C for D {
+    fn name(&self) {
+        println!("B for D {:?}", self);
+    }
 }
 
 fn main() {
-    let mut number = 13;
-
-    inc(&mut number);
-
-    println!("Tell me about {}", number);
+    let o = D::new();
+    //o.name();
+    A::name(&o);
+    B::name(&o);
+    C::name(&o);
 }
